bd2ea533f851149ed09c74ba3ffe0baa3c5ae5b0
skybound0532 <tianchenggu@outlook.com>
2024-01-17 16:57:45 -0600

    Migrate to Arduino IDE

--- Software/CompetitionScript4/CompetitionScript4.ino
+++ Software/CompetitionScript4/CompetitionScript4.ino
@@ -1,15 +1,22 @@
 
 // Packages
+#include "bsp_api.h"
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+Adafruit_MotorShield motorShield = Adafruit_MotorShield();
+Adafruit_DCMotor *motorLeft = motorShield.getMotor(1); 
+Adafruit_DCMotor *motorRight = motorShield.getMotor(3); 
+
 void setup()
 {
-
+    
+    motorShield.begin();
+    motorLeft->setSpeed(255);
 }
 
 void loop()
 {
-
+    motorLeft->run(FORWARD);
 }
